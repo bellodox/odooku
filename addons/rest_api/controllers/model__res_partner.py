41dcdadf41b384f64c9aa43814ffacb5130978d8
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2018-05-22 21:31:04 -0500

    add account_number and last_contract to api

--- addons/rest_api/controllers/model__res_partner.py
+++ addons/rest_api/controllers/model__res_partner.py
@@ -52,6 +52,8 @@ OUT__res_partner__read_all__JSON = (                # editable
     'copago_amount',
     'outstanding',
     'group_code',
+    'account_number',
+    'last_contract',
 
 
 )
@@ -87,6 +89,8 @@ OUT__res_partner__read_one__JSON = (                # editable
     'copago_amount',
     'outstanding',
     'group_code',
+    'account_number',
+    'last_contract',
     # Address of client
     ('child_ids', [(
         'id',
